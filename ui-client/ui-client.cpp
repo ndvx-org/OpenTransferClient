e29980476b2b1310f5ce463b868659eddeed490d
Paw all obo alexaro 0 <paw.all.obo.alexaro@gmail.com>
2026-07-12 17:28:36 +0200

    Add: CMake Project (ui-client)

---
+++ ui-client/ui-client.cpp
@@ -0,0 +1,12 @@
+﻿// ui-client.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "ui-client.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
